d05beddff9270c693fedf222245df003f7c0dab7
icmx <32983315+icmx@users.noreply.github.com>
2025-07-19 15:28:18 +0600

    update angular to v20, was v19

--- src/app/common/providers/clipboard.provider.ts
+++ src/app/common/providers/clipboard.provider.ts
@@ -1,5 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { inject, InjectionToken, Provider } from '@angular/core';
+import { inject, InjectionToken, Provider, DOCUMENT } from '@angular/core';
 
 export const CLIPBOARD = new InjectionToken<Clipboard>('CLIPBOARD');
 
