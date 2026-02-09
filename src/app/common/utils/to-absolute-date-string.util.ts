473327d70fce515d5bf3d6f1bb43557918a96160
icmx <32983315+icmx@users.noreply.github.com>
2026-02-09 19:31:52 +0600

    better dates handling

--- src/app/common/utils/to-absolute-date-string.util.ts
+++ src/app/common/utils/to-absolute-date-string.util.ts
@@ -1,5 +1,5 @@
 import { DateInit } from '../types/date-init.type';
 
 export const toAbsoluteDateString = (init: DateInit): string => {
-  return new Date(init).toJSON().replace('T', ' ').slice(0, 16);
+  return new Date(init).toJSON()?.replace('T', ' ').slice(0, 16) || '';
 };
